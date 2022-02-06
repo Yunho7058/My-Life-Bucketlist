4256daccab425c0306e774fc6fa28318928b0145
yunho <Yunho7058@github.com>
2022-02-06 19:48:45 +0900

    UI 구현

--- client/src/Page/Login.js
+++ client/src/Page/Login.js
@@ -1,5 +1,7 @@
 'use strict';
+import { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const LoginBack = styled.div`
   background-color: rgba(0, 0, 0, 0.4);
@@ -29,20 +31,79 @@ const LoginInput = styled.input`
 
 const Btn = styled.button``;
 
+const ErrMsg = styled.div`
+  color: red;
+`;
+
+const ClossBtn = styled.span`
+  cursor: pointer;
+  margin: 10px;
+`;
+
 function Login(props) {
+  const [loginInfo, setLoginInfo] = useState({
+    user_id: '',
+    user_password: '',
+  });
+
+  const handleLoginBtnClick = () => {
+    let url = '';
+    if (!(loginInfo.user_id, loginInfo.user_password)) {
+      return setErrMsg('아이디와 비밀번호를 입력 해주세요.');
+    } else {
+      axios
+        .post(url, loginInfo, {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: true,
+        })
+        .then((res) => {
+          // 1. 로컬스토리지에 토큰 저장(왜 토큰을 저장할까???)
+          localStorage.setItem('accToken', res.data.accessToken);
+          // 2. 모달 창 꺼지고
+          props.setIsSignupModal(false);
+          // 3. 로그인 상태로 변경
+          props.setIsLogin(true);
+        })
+        .catch((err) => {
+          setErrMsg('아이디 또는 비밀번호과 틀립니다.');
+          console.log(err, '로그인 err');
+        });
+    }
+  };
+
+  const [errmsg, setErrMsg] = useState('');
+
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value.toLowerCase() });
+    console.log(loginInfo);
+  };
+
+  const enterKey = (e) => {
+    if (e.key === 'Enter') return handleLoginBtnClick();
+  };
+
   return (
     <LoginBack onClick={props.loginClick}>
       <LoginView onClick={(e) => e.stopPropagation()}>
         로그인
+        <ClossBtn onClick={props.loginClick}>&times;</ClossBtn>
         <div>
-          <LoginInput type="id" placeholder="아디를 입력하세요."></LoginInput>
+          <LoginInput
+            type="id"
+            onChange={handleInputValue('user_id')}
+            onKeyPress={enterKey}
+            placeholder="아디를 입력하세요."
+          ></LoginInput>
           <LoginInput
             type="password"
+            onChange={handleInputValue('user_password')}
+            onKeyPress={enterKey}
             placeholder="비밀번호를 입력하세요."
           ></LoginInput>
         </div>
-        <Btn>로그인</Btn>
-        <Btn>회원가입</Btn>
+        <ErrMsg>{errmsg}</ErrMsg>
+        <Btn onClick={handleLoginBtnClick}>로그인</Btn>
+        <Btn onClick={props.signupClick}>회원가입</Btn>
         <div>
           <Btn>카카오톡으로 로그인</Btn>
         </div>
