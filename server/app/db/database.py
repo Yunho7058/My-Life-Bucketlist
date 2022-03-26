0d73bb6b0ee3aa7fd9a9038f78905817ea6b1c4a
kiung <kiung22@naver.com>
2022-03-26 17:53:23 +0900

    fix: database url에 charset 지정

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f"mysql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:3306/{settings.DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:3306/{settings.DATABASE_NAME}?charset=utf8"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
