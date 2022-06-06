55ce38be8e0b0d78de999bed881889ab9430f001
kiung <kiung22@naver.com>
2022-06-06 22:43:19 +0900

    fix: event 에러

--- server/app/models/users.py
+++ server/app/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, event
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.attributes import get_history
 
